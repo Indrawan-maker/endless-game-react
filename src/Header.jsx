f40b289e4deda204a12fdafea867e8043b376b41
Indrawan <indrawanismail0@gmail.com>
2025-07-12 02:33:26 +0800

    add feature header

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export default function () {
+    return (
+        <header>
+    <h2>Assembly: Endgame</h2>
+    <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+    {/* <p className="comment">Doh! You lose! Too many misses. Good luck next time.</p> */}
+    </header>
+    )
+}
